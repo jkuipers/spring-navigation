a82ad8687f1fef3d126301219af004ce7d5b85d0
Quinten Krijger <qkrijger@gmail.com>
2014-05-09 16:04:17 +0200

    Limited scope of NavigationStack, since the associated functionality should be exposed through the enricher

--- src/main/java/nl/trifork/spring/navigation/stack/NavigationStack.java
+++ src/main/java/nl/trifork/spring/navigation/stack/NavigationStack.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * @author Quinten Krijger
  */
-public class NavigationStack {
+class NavigationStack {
 
     private List<String> navigationSteps = new ArrayList<>();
 
